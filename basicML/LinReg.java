f84d0a955b835152a4bae27f9cf66a6b5cee6cc9
dillonallen92 <dillon.allen.92@gmail.com>
2018-01-19 10:25:45 -0800

    Add files via upload

---
+++ basicML/LinReg.java
@@ -0,0 +1,58 @@
+
+
+// Linear Regression Formula
+class LinReg
+{
+  private final double intercept, slope, r2;
+
+  public LinReg(double[] x, double [] y)
+  {
+    if (x.length != y.length)
+    {
+      throw new IllegalArgumentException("Array lengths not equal");
+    }
+
+    int n = x.length;
+
+    double sumx = 0.0, sumy = 0.0, b1num = 0.0, b1denom = 0.0, b1 = 0.0, b0 = 0.0;
+
+    for(int i=0; i < n; i++)
+    {
+      sumx += x[i];
+      sumy += y[i];
+    }
+
+    double xbar = 0.0, ybar = 0.0, ssto = 0.0, ssr = 0.0;
+    xbar = (sumx/n);
+    ybar = (sumy/n);
+
+    for(int i=0; i < n; i++)
+    {
+      b1num += (x[i] - xbar)*(y[i] - ybar);
+      b1denom += Math.pow((x[i] - xbar),2);
+      ssto += Math.pow((y[i] - ybar),2);
+    }
+
+    b1 = (b1num / b1denom);
+
+    b0 = ybar - b1*xbar;
+
+    slope = b1;
+    intercept = b0;
+
+    // Calculate the SSR
+    for(int i = 0; i < n; i++)
+    {
+      ssr += Math.pow(((intercept + slope*x[i]) - ybar),2);
+    }
+
+    r2 = (ssr/ssto) ;
+  }
+
+  public String dataReport()
+  {
+    return "The regression line is y_hat = " + intercept + " + " + slope + "* x" + " with an R2 of " + r2;
+
+  }
+
+}
