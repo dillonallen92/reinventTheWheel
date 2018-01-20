245c46d1a0cb628d73b24e7f42d31754cf4f65da
dillonallen92 <dillon.allen.92@gmail.com>
2018-01-19 22:37:10 -0800

    Added training, test set functionality, predict function

--- basicML/RegressionCalculator.java
+++ basicML/RegressionCalculator.java
@@ -21,12 +21,32 @@ public class RegressionCalculator
 
     int x_train_size =0, y_train_size = 0, x_test_size = 0, y_test_size = 0;
 
-    x_train_size = Math.ceil(0.8*x.length).intValue();
+    // Split into test and train sets. Take one away from each to keep the indicies restricted to the bounds
+
+    x_train_size = (int) Math.ceil(0.8*xdata.length);
+    y_train_size = (int) Math.ceil(0.8*ydata.length);
+
+    // Use Arrays.copyOfRange to get our train and test sets
+
+    double[] x_train = Arrays.copyOfRange(xdata, 0, x_train_size);
+    double[] y_train = Arrays.copyOfRange(ydata, 0, y_train_size);
+
+    double[] x_test = Arrays.copyOfRange(xdata, x_train_size, xdata.length);
+    double[] y_test = Arrays.copyOfRange(ydata, y_train_size, ydata.length);
+
+    // Display our test and training sets
+
+    System.out.println("Our xtrain set is " + Arrays.toString(x_train));
 
 
-    Regressor regressor = new LinReg(xdata, ydata);
-    System.out.println(regressor.dataReport());
+    // Call the regressor on our training data
 
+    Regressor regressor = new LinReg(x_train, y_train);
+    System.out.println("The training set yields the results " + "\n" + regressor.dataReport());
 
+    // Call the predictor method now to see what we get
+
+    double y_pred[] = regressor.predict(x_test);
+    System.out.println("The test set predictions are " + "\n" + Arrays.toString(y_pred));
   }
 }
